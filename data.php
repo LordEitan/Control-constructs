7565dfcc7cc061d677cca749cda82696203f0589
LordEitan <mestechko11@outlook.com>
2020-08-13 18:39:26 +0300

    Output information on screen

---
+++ data.php
@@ -0,0 +1,19 @@
+<?php
+
+$articles = [
+    [
+        'title' => 'Best gaming mouse',
+        'description' => 'So if you want to win in game, check this mouse',
+        'image' => 'Razer3.jpg',
+    ],
+    [
+        'title' => 'Best gaming mouse',
+        'description' => 'So if you want to win in game, check this mouse',
+        'image' => 'Razer3.jpg',
+    ],
+    [
+        'title' => 'Best gaming mouse',
+        'description' => 'So if you want to win in game, check this mouse',
+        'image' => 'Razer3.jpg',
+    ]
+];
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once "data.php";
+
+include_once "main_page.php";
